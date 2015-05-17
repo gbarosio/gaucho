921c806591c5ffe071ae2b973f1da636b999dff8
Guido Barosio <guido.barosio@groupon.com>
2015-05-17 19:32:20 -0300

    execution logic

--- system/core/Gaucho.pl
+++ system/core/Gaucho.pl
@@ -1,3 +1,34 @@
 #!/usr/bin/perl
 
-# Common.pl 
+# Gaucho.pl
+
+use strict;
+
+my @version = ('0.01-dev','Gaucho Version');
+
+
+&main();
+
+sub main() {
+	&Constants;
+	&Common;
+	&Exceptions;
+	&Hooks;
+	&Config;
+	&URI;
+	&Router;
+	&Output;
+	&Controller;
+	&Post;
+}
+
+sub Constants { } 
+sub Common { } 
+sub Exceptions {  }
+sub Hooks { } 
+sub Config { } 
+sub URI { }
+sub Router { } 
+sub Output { } 
+sub Controller { } 
+sub Post { } 
