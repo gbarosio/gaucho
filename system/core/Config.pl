b0a2aeeceb3ca0614ace2c08cfac8306f8e4bcce
Guido Barosio <gbarosio@gmail.com>
2016-04-06 17:29:28 +0530

    implementing YAML::XS for parsing configurations

--- system/core/Config.pl
+++ system/core/Config.pl
@@ -1,3 +1,11 @@
 #!/usr/bin/perl
+use YAML::XS 'LoadFile';
+use Data::Dumper;
+use strict;
 
-# Common.pl 
+my $config = LoadFile('../../app/config/config.cfg');
+
+print $config->{projectName}."\n";
+print $config->{version}."\n";
+print $config->{base_url}."\n";
+print $config->{log_path}."\n";
